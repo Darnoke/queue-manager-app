afe396a24386636101e86ae1a9ed3893afe71da5
Konrad <konradwozniak64@gmail.com>
2023-12-02 20:03:56 +0100

    Added confimation dialog

---
+++ src/dialogs/ConfirmationDialog.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+
+interface ConfirmationDialogProps {
+  open: boolean;
+  onClose: (answer: boolean) => void;
+  messageInput: string;
+}
+
+const ConfirmationDialog: React.FC<ConfirmationDialogProps> = ({ open, onClose, messageInput = '' }) => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    setMessage(messageInput);
+  }, [open]);
+
+
+  return (
+    <Dialog open={open}>
+      <DialogTitle>Change Password</DialogTitle>
+      <DialogContent>
+        <p>{message}</p>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => onClose(false)}>Cancel</Button>
+        <Button onClick={() => onClose(true)} color="primary">
+          Confirm
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ConfirmationDialog;
