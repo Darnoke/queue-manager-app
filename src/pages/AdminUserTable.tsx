bb13553cb412efb0cec2e59f16f6487113cf4a7f
Konrad <konradwozniak64@gmail.com>
2023-12-02 20:32:48 +0100

    Added delete user

--- src/pages/AdminUserTable.tsx
+++ src/pages/AdminUserTable.tsx
@@ -3,11 +3,14 @@ import { User } from '../models/User';
 import './AdminUserTableStyles.scss';
 import { Button } from '@mui/material';
 import CreateUserDialog from '../dialogs/CreateUserDialog';
+import ConfirmationDialog from '../dialogs/ConfirmationDialog';
 
 const AdminUserTable = () => {
   const [userData, setUserData] = useState<User[]>([]);
   const [loading, setLoading] = useState(true);
   const [isCreateUserDialogOpen, setIsCreateUserDialogOpen] = useState(false);
+  const [isConfirmDialogOpen, setIsConfirmDialogOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState('');
   const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
 
   const fetchUserData = async () => {
@@ -19,6 +22,19 @@ const AdminUserTable = () => {
     } catch (error) {
       console.error('Error fetching user data:', error);
       setLoading(false);
+    }
+  }
+
+  const deleteUser = async (username: string) => {
+    try {
+      setSelectedUser('');
+      const response = await fetch(apiUrl + '/admin/users/' + username, {method: 'DELETE', credentials: 'include'});
+
+      if (response.ok) {
+        fetchUserData();
+      }
+    } catch (error) { 
+      console.error('Error while deleting user:', error);
     }
   }
 
@@ -36,6 +52,16 @@ const AdminUserTable = () => {
 
   const openCreateUserDialog = () => {
     setIsCreateUserDialogOpen(true);
+  }
+
+  const deleteDialogCallback = (answer: boolean) => {
+    setIsConfirmDialogOpen(false);
+    if (answer) deleteUser(selectedUser);
+  }
+
+  const onDeleteUser = (username: string) => {
+    setSelectedUser(username);
+    setIsConfirmDialogOpen(true);
   }
 
   return (
@@ -57,12 +83,14 @@ const AdminUserTable = () => {
                   <tr key={user.username}>
                     <td>{user.username}</td>
                     <td>{user.role}</td>
+                    <td><Button onClick={() => onDeleteUser(user.username)}>Delete</Button></td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
           <CreateUserDialog open={isCreateUserDialogOpen} onClose={(param: boolean) => onCreateUserClose(param)}/>
+          <ConfirmationDialog open={isConfirmDialogOpen} onClose={(answer: boolean) => deleteDialogCallback(answer)} messageInput={'Delete user: ' + selectedUser + '?'}/>
           </>
       )}
     </div>
