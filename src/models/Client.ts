d002c59e8326c54ba4f08994f03a2a8d201a1eba
Konrad <konradwozniak64@gmail.com>
2024-01-04 15:22:28 +0100

    Added webSockets

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -1,6 +1,7 @@
 import { ClientStatus } from "../enums/ClientStatus";
 
 export interface Client {
+  _id: string,
   assignedNumber: number,
   category: string,
   createdAt: Date,
--- src/pages/Worker/WorkerQueueManagmentComponent.tsx
+++ src/pages/Worker/WorkerQueueManagmentComponent.tsx
@@ -2,21 +2,53 @@ import React, { useEffect, useState } from 'react';
 import { useUser } from '../../contexts/UserContext';
 import { Client } from '../../models/Client';
 import { useParams } from 'react-router-dom';
+import io, { Socket } from 'socket.io-client';
 
 const WorkerQueueManagmentComponent: React.FC = () => {
   const [clients, setClients] = useState<Client[]>([]);
+  const [socket, setSocket] = useState<Socket | null>(null);
 
   const { queueId } = useParams();
   const { user } = useUser();
 
   useEffect(() => {
+    // Connect to the Socket.IO server
+    const socket = io('http://localhost:3000', {
+      query: {
+        queueId: queueId,
+      }
+    });
+    setSocket(socket);
 
-  }, []);
+    console.log(socket)
+
+    // Event listener for receiving clients from the server
+    socket.on('on_connect', (newClients: Client[]) => {
+      console.log(newClients)
+      setClients(newClients);
+    });
+
+    socket.on('new_client_added', (newClient: Client) => {
+      console.log(newClient)
+      setClients(prevClients => [...prevClients, newClient]);
+    })
+
+    // Clean up the socket connection when the component unmounts
+    return () => {
+      if (socket) {
+        console.log('disconnect')
+        socket.disconnect();
+      }
+    };
+  }, [queueId]); // Only run this effect on mount and unmount
 
   return (
     <div>
       <h3>Clients</h3>
       <div className='client-list'>
+        {clients.map(client => (
+          <div key={client._id}>{client.assignedNumber}</div>
+        ))}
       </div>
     </div>
   );
