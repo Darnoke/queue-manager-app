851f2bdfff06fa34d51f5a432831d3fcaa762e14
Konrad <konradwozniak64@gmail.com>
2024-01-16 15:15:15 +0100

    Update redirect after login

--- src/routes/RedirectToAdmin.tsx
+++ src/routes/RedirectToAdmin.tsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom"
 
 const RedirectToAdmin = () => {
-    return <Navigate to='/admin/plan'/>
+    return <Navigate to='/admin/users'/>
 }
 
 export default RedirectToAdmin;
