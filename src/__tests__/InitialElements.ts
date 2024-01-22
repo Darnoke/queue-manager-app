232c60349d489c69efd2da0b538170ab1026c7c6
Konrad <konradwozniak64@gmail.com>
2024-01-22 21:27:46 +0100

    Fixed whitespace

--- src/__tests__/InitialElements.ts
+++ src/__tests__/InitialElements.ts
@@ -1,33 +1,33 @@
 import { Edge, Node } from "reactflow";
 
 export const nodes: Node[] = [
-    {
-      id: '1',
-      type: 'start',
-      position: { x: 250, y: 0 },
-      data: {},
+  {
+    id: '1',
+    type: 'start',
+    position: { x: 250, y: 0 },
+    data: {},
+  },
+  {
+    id: '2',
+    type: 'question',
+    dragHandle: '.custom-drag-handle',
+    position: { x: 100, y: 200 },
+    data: {
+      question: "This is a test",
+      answers: [
+          {
+              id: 'ans-0',
+              answer: 'test 0'
+          },
+          {
+              id: 'ans-1',
+              answer: 'test 1'
+          },
+      ]
     },
-    {
-      id: '2',
-      type: 'question',
-      dragHandle: '.custom-drag-handle',
-      position: { x: 100, y: 200 },
-      data: {
-        question: "This is a test",
-        answers: [
-            {
-                id: 'ans-0',
-                answer: 'test 0'
-            },
-            {
-                id: 'ans-1',
-                answer: 'test 1'
-            },
-        ]
-      },
-    },
+  },
 ]
 
 export const edges: Edge[] = [
-    { id: 'e1-2', source: '1', target: '2'},
-  ];
+  { id: 'e1-2', source: '1', target: '2'},
+];
