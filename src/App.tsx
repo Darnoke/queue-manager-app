bed975b5c6084e74889923fd70f2f963f0acf677
Konrad <konradwozniak64@gmail.com>
2024-01-22 20:11:25 +0100

    Moved UserProvider and Router to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import './App.scss'
 import AppRouter from './routes/AppRouter'
 import theme from './styles/theme.tsx'
 import './styles/margins.scss'
+import { UserProvider } from './contexts/UserContext.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
 
   return (
   <ThemeProvider theme={theme}>
-    <AppRouter />
+    <UserProvider>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </UserProvider>
   </ThemeProvider>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { UserProvider } from './contexts/UserContext.tsx'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <UserProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </UserProvider>
+    <App />
   </React.StrictMode>,
 )
